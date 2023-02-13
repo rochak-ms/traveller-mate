75477d60d451915db8314907dc10502c6b142096
rocky <rochak.ms@gmail.com>
2023-02-13 19:08:04 +0800

    update server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,16 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
