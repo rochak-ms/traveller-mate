cc1b9ae4bce567eb3f8f317d4960d2af965e3167
rocky <rochak.ms@gmail.com>
2023-02-14 21:24:59 +0800

    update server.js2

--- server/server.js
+++ server/server.js
@@ -16,9 +16,13 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://traveller-mate.herokuapp.com/"
+  );
   res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
